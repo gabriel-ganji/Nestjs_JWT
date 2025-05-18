b2f7a1334fb413c8763e6ce603c5d9baad3c331e
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-05-18 15:09:13 -0300

    nestjs/config and configuring .env envoirement

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,18 +13,20 @@ import { MessageModule } from './message/message.module';
 import { IsAdminGuard } from './common/guards/is-admin.guard';
 import { SimpleMiddleware } from './common/middlewares/simple.middleware';
 import { ErrorExceptionFilter } from './common/filters/error-exception.filter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      database: 'postgres',
-      password: '123456',
-      autoLoadEntities: true, //Charge entitites without specifications.
-      synchronize: true, //Synchronize with DB(DONT USE ON PRODUTION!!!).
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      database: process.env.DATABASE_DATABASE,
+      password: process.env.DATABASE_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES), // Carrega entidades sem precisar especifica-las
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Sincroniza com o BD. Não deve ser usado em produção
     }),
     MessageModule,
     PersonModule,

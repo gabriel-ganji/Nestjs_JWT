4198018a4567f206a396cd89c58fc9e293a8730e
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-08-24 16:26:33 -0300

    feat: creating AuthService and AuthController to the user login route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,19 @@
 import { Global, Module } from "@nestjs/common";
 import { HashingService } from "./hashing/hashing.service";
 import { BcryptService } from "./hashing/bcrypt.service";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 @Global()
 @Module({
-    providers: [{
-        provide: HashingService,
-        useClass: BcryptService,
-    }],
+    controllers: [AuthController],
+    providers: [
+        {
+            provide: HashingService,
+            useClass: BcryptService,
+        },
+        AuthService,
+    ],
     exports: [HashingService],
 })
 export class AuthModule { }

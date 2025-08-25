942d44bf82cc4b4d924a7eb64510475184e302f2
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-08-25 16:05:23 -0300

    fix: right nomenclature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -44,7 +44,7 @@ export class AuthService {
             throw new UnauthorizedException('Usuário ou senha inválida');
         }
 
-        const acessToken = await this.jwtService.signAsync(
+        const accessToken = await this.jwtService.signAsync(
             {
                 sub: person.id,
                 email: person.email,
@@ -58,7 +58,7 @@ export class AuthService {
         )
 
         return {
-            acessToken 
+            accessToken 
         };
 
     }
